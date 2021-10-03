1418eb2fe68e6dfd8817103f9ca0de7cf0b5b95a
Jaime Gómez <91792957+gomezrj@users.noreply.github.com>
2021-10-03 12:28:09 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const menu = document.querySelector('.header .navbar .menu');
 const mobile_menu = document.querySelector('.header .navbar ul');
 const header = document.querySelector('.header.container');
 
+const atHome = document.querySelector('.header .navbar ul li:first-child a')
+
 menu.addEventListener('click',()=>{
   menu.classList.toggle('active');
   mobile_menu.classList.toggle('active');
 })
 
-document.addEventListener('scroll',()=>{
-  var scroll_position = window.scrollY;
-  if(scroll_position > 600){
-    header.style.backgroundColor = "#525252";
-  }
-  else{
-    header.style.backgroundColor = "transparent";
-  }
-})
+if(!atHome.classList.contains('activepage')){
+  header.style.backgroundColor = "#000000";
+}
+else{
+  document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 600){
+      header.style.backgroundColor = "#000000";
+    }
+    else{
+      header.style.backgroundColor = "transparent";
+    }
+  })
+}
